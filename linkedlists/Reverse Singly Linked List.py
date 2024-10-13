e39fe0b731f6e46dd1b4fade9c929138d52e51a0
neel2435 <neeldoshi2435@gmail.com>
2024-10-13 00:28:24 -0700

    add comments to reverse linked list

--- linkedlists/Reverse Singly Linked List.py
+++ linkedlists/Reverse Singly Linked List.py
@@ -3,9 +3,28 @@ class Solution:
         curr, temp, prev = head, None, None
 
         while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
+            temp = curr.next # save the next
+            curr.next = prev # point the first node to None (1 -> None)
+            prev = curr # save current node
+            curr = temp # iterate the current node
+
+        # for [1, 2, 3, 4]
+        
+        # temp = 2
+        # 1 points to None
+        # curr node becomes 2
+
+        # temp = 3
+        # 2 points to 1 points to None
+        # curr node becomes 3
+
+        # temp = 4
+        # 3 points to 2 points to 1 points to None
+        # curr node becomes 4
+
+        # temp = None (end of list)
+        # 4 points to 3 points to 2 points to 1 points to None
+        # curr node becomes None and breaks the loop
+
         return prev
 
